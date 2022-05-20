a7e2c507a99f50dba850c9133ea2095a759ff9fc
ddolan88 <104475059+ddolan88@users.noreply.github.com>
2022-05-20 10:45:39 -0700

    simple calculator

---
+++ store.js
@@ -0,0 +1,9 @@
+console.log("store");
+
+let quantity= (prompt("Enter your quantity:"));
+let tax = .8;
+let Number= (price = 5.99);
+let total = price*quantity*tax;
+
+document .write(`<div class="container">YOUR TOTAL IS: $${total}`)
+
